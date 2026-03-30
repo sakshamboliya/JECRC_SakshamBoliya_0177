adb367d8ffc77df28f8ec2438ac8b0bf5d70120a
Saksham <sakshamboliyait26@gmail.com>
2026-03-30 11:26:43 +0530

    Assignment added

---
+++ Week_7_React/30-03-26/Assesment1final/dark_mode/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ThemeToggle from './themetoggle';
+
+function App() {
+  return (
+    <div>
+      <ThemeToggle />
+    </div>
+  );
+}
+
+export default App;
+
+
